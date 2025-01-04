49904a8e4280222eee71bb46729ddf3603151b8d
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-01-04 23:15:36 +0600

    contact section created

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,59 @@ tabs.forEach((tab) => {
 		tab.classList.add("tab__active");
 	});
 });
+
+/* =================Contact Form=============== */
+const contactForm = document.getElementById("contact-form"),
+	contactName = document.getElementById("contact-name"),
+	contactEmail = document.getElementById("contact-email"),
+	contactSubject = document.getElementById("contact-subject"),
+	contactMessage = document.getElementById("contact-message"),
+	errorMessage = document.getElementById("error-message");
+
+const sendEmail = (e) => {
+	e.preventDefault();
+
+	// check if the field has a value
+	if (
+		contactName.value === "" ||
+		contactEmail.value === "" ||
+		contactSubject.value === "" ||
+		contactMessage.value === ""
+	) {
+		// show message
+        errorMessage.className = "error__message first-color";
+		errorMessage.textContent = "Write all the input fields";
+	} else {
+		// serviceID - templateID - #from - publickey
+		emailjs
+			.sendForm(
+				"service_hsnswfl",
+				"template_pd6tlro",
+				"#contact-form",
+				"6MIdtoOc2Djr19XyI"
+			)
+			.then(
+				() => {
+					// show message and add color, window + dot to open emoji
+					errorMessage.className = "error__message color-green";
+					errorMessage.innerText = "Message sent";
+
+					// remove message after 3 seconds
+					setTimeout(() => {
+						errorMessage.innerText = "";
+					}, 5000);
+				},
+				(error) => {
+					alert("OOPS! SOMETHING WENT WRONG...", error);
+				}
+			);
+
+		// clear input fields
+		contactName.value = "";
+		contactEmail.value = "";
+		contactSubject.value = "";
+		contactMessage.value = "";
+	}
+};
+
+contactForm.addEventListener("submit", sendEmail);
